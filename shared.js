c4e2469dbcbe2d0dce09c8cb3e11ade16b871c1b
Louis Pisani <louispisani@Louiss-MBP.home>
2020-12-04 11:44:37 -0500

    formatting

--- shared.js
+++ shared.js
@@ -4,22 +4,26 @@ var positionSmall = 0;
         console.log({positionSmall})
         if (positionSmall > 300) {
             $(".linkitem").css('color', 'transparent');
+            $(".linkitem:hover span").css('border-bottom', 'none');
             $(".linkitem.schedule").css('border', 'none');
         }
          else {
             $(".linkitem").css('color', 'white');
+            $(".linkitem:hover span").css('border-bottom', '1px solid #fff');
             $(".linkitem.schedule").css('border', '1px solid white');
         }
 
         if (positionSmall >800) {
             $("#logo").css('opacity', '0');
+            $('.menu__btn').css('background-color',"#8ca48c").css('opacity','0.6');
         }
          else {
             $("#logo").css('opacity', '1');
+            $('.menu__btn > span,.menu__btn >span::before,.menu__btn > span::after').css('background-color',"#fff");
+            $('.menu__btn').css('background-color',"transparent").css('opacity','1');
 
         }
-        $('.menu__item').hover(function () {
-            $('.menu__item').not(this).removeClass('active');
-            $(this).addClass('active');
-        });
+   
+
     });
+
